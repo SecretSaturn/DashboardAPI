5d40ce01dc2c9ca994bf246181ecc620d5b89868
h3rt <94856309+SecretSaturn@users.noreply.github.com>
2024-04-20 16:32:24 +0200

    update

--- src/server.ts
+++ src/server.ts
@@ -4,23 +4,53 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+// Define a function to fetch token configuration
+async function fetchTokenConfig() {
+    const url = "https://raw.githubusercontent.com/scrtlabs/dash.scrt.network/master/public/PriceIdsString.txt";
+    const response = await fetch(url);
+    const text = await response.text();
+    return text
+}
+
+// Store tokens globally
+let tokens: any;
+let prices: any;
+
+// Initialize the token configuration on server start
+fetchTokenConfig().then(config => {
+    console.log(config)
+    tokens = config;
+    console.log("Token configuration loaded.");
+    console.log(tokens)
+    // Start the price refresh interval
+    refreshPrices()
+    setInterval(refreshPrices, 30000); // 30000 milliseconds == 30 seconds
+}).catch(error => {
+    console.error("Failed to load token configuration:", error);
 });
 
-app.get("/getPrices", async (req, res) => {
-  try {
-  
+const refreshPrices = async () => {
+    try {
+        const response = await fetch(`https://api.coingecko.com/api/v3/simple/price?ids=${tokens}&vs_currencies=USD`);
+        prices = await response.json();
+        console.log(prices)
+        console.log("Prices refreshed.");
+    } catch (error) {
+        console.error("Error fetching prices:", error);
+    }
+};
 
-    console.log(result)
-    return res.json(result);
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
 
-  } catch (error: any) {
-    console.error("Error querying data:", error);
-    return res.status(500).send(error.message);
-  }
+app.get("/getPrices", (req, res) => {
+    if (!prices) {
+        return res.status(503).send("Price data not initialized.");
+    }
+
+    res.json(prices);
 });
 
