26fe2b1c3e6b2634bd5c10a02c19a96c80df5e0b
h3rt <94856309+SecretSaturn@users.noreply.github.com>
2024-04-22 01:48:15 +0200

    add Volume

--- src/server.ts
+++ src/server.ts
@@ -11,16 +11,20 @@ async function fetchTokenConfig() {
     const url = "https://raw.githubusercontent.com/scrtlabs/dash.scrt.network/master/public/PriceIdsString.txt";
     const response = await fetch(url);
     tokens = await response.text();
+    console.log("Token configuration loaded.");
 }
 
 // Store tokens globally
 let tokens: any;
 let prices: any;
 let currencies: any;
+let volume: any;
+let Chart1Day: any;
+let Chart1Month: any;
+let Chart1Year: any;
 
 // Initialize the token configuration on server start
 fetchTokenConfig().then(() => {
-    console.log("Token configuration loaded.");
     // Start the price refresh interval
     refreshData()
     setInterval(refreshData, 60*1000); // 60000 milliseconds == 60 seconds
@@ -31,11 +35,12 @@ fetchTokenConfig().then(() => {
 
 const refreshData = async () => {
     try {
-        const response = await fetch(`https://api.coingecko.com/api/v3/simple/price?ids=${tokens}&vs_currencies=USD`);
-        prices = await response.json();
-        const response2 = await fetch(`https://api.coingecko.com/api/v3/simple/price?ids=usd&vs_currencies=eur,jpy,gbp,aud,cad,chf`);
-        currencies = await response2.json();
-        console.log("Data refreshed.");
+        prices = await(await fetch(`https://api.coingecko.com/api/v3/simple/price?ids=${tokens}&vs_currencies=USD`)).json();
+        currencies = await(await fetch(`https://api.coingecko.com/api/v3/simple/price?ids=usd&vs_currencies=eur,jpy,gbp,aud,cad,chf`)).json();
+        volume = await(await fetch(`https://api.coingecko.com/api/v3/simple/price?ids=secret&vs_currencies=usd,eur,jpy,gbp,aud,cad,chf&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true`)).json();
+       // Chart1Day = await(await fetch(`https://api.coingecko.com/api/v3/simple/price?ids=usd&vs_currencies=eur,jpy,gbp,aud,cad,chf`)).json();
+        //Chart1Month = await(await fetch(`https://api.coingecko.com/api/v3/simple/price?ids=usd&vs_currencies=eur,jpy,gbp,aud,cad,chf`)).json();
+        //Chart1Year = await(await fetch(`https://api.coingecko.com/api/v3/simple/price?ids=usd&vs_currencies=eur,jpy,gbp,aud,cad,chf`)).json();
     } catch (error) {
         console.error("Error fetching aata:", error);
     }
@@ -58,6 +63,14 @@ app.get("/getCurrencies", (req, res) => {
         return res.status(503).send("Currency data not initialized.");
     }
     res.json(currencies);
+});
+
+app.get("/getVolume", (req, res) => {
+    if (!volume) {
+        return res.status(503).send("Volume data not initialized.");
+    }
+
+    res.json(volume);
 });
 
 
